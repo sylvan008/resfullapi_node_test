0f9a3bd6fcc0f3db430aeb0410721c3694e096ad
sylvan008 <sylvan.climb@gmail.com>
2021-11-18 13:31:26 +0300

    Adds getting and parsing url path

--- index.js
+++ index.js
@@ -4,12 +4,25 @@
 
 // dependencies
 const http = require('http');
+const { URL } = require('url');
 
 const HOST = 'localhost';
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
+	// Get the url
+	const baseUrl = `http://${req.headers.host}/`;
+	const reqUrl = new URL(req.url, baseUrl);
+
+	// Get the path
+	const path = reqUrl.pathname;
+	const trimmedPath = path.replace(/^\/|\/$/g, '');
+
+	// Send the response
 	res.end('Hello!\n');
+
+	// Log the request path
+	console.log(`Request received on path: ${trimmedPath}`);
 });
 
 server.listen(PORT, HOST, () => {
